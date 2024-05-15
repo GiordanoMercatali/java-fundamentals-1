7c83e1ccbff7e1f3884d182451511864a7db74aa
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-05-15 14:40:51 +0200

    first exercise

---
+++ src/Ticket.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Ticket {
+    public static void main(String[] args) throws Exception {
+        Scanner myScanner = new Scanner(System.in);
+
+        //Kilometers
+        System.out.print("Insert how kilometers you wanna travel: ");
+        int km = myScanner.nextInt();
+        System.out.println("You'll run " + km + " kilometers.");
+
+        //Age
+        System.out.print("Insert age (discounts for under 18 and over 65): ");
+        int age = myScanner.nextInt();
+        System.out.println("You are " + age + " years old.");
+        
+        //Ticket Price
+        double price = (double)km * 0.21;
+
+        if(age < 18){
+            price = price * 0.8;
+        } else if(age > 65){
+            price = price * 0.6;
+        }
+
+        System.out.print("Your ticket costs " + price);
+    }
+} 
